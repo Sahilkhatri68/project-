30738ec89c9107ad22cf2e2da617cb5e19552773
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2021-06-26 10:01:47 +0530

    second commit

---
+++ usip.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class usip {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        int num1 = in.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(num1 + " x " + (i + 1) + " = " + (num1 * (i + 1)));
+        }
+    }
+}
